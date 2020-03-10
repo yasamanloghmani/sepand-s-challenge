c9a18ec570aa8ecf3c6283337eb327c36936f9fb
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-03-10 16:30:30 -0400

    edit

--- main.js
+++ main.js
@@ -2,25 +2,21 @@ const basePrice = 24000;
 let configuration = 0;
 let docking = 1200;
 let addOnPrice = 0;
-// let taxPrice = 0;
 let input = [];
 
 function configurationPrice() {
     configuration += basePrice;
-    for(let i=0; i<input.length;i++){
-        
+    for(let i=0; i<input.length; i++){
         if(addOnPrice < 8000){
             addOnPrice += input[i].price;
         }
         else {
             addOnPrice += input[i].price / 2 ;
-            }
-        
+        }
     }
     configuration += addOnPrice;
     docking += (configuration * 2) / 100;
     configuration += docking;
-     
 }
 function priceCalculater() {
     configurationPrice();
@@ -28,9 +24,7 @@ function priceCalculater() {
     let finalPrice = configuration + taxPrice;
     console.log(`The cost for this car is ${finalPrice}`)
 }
-
 priceCalculater();
-
 input = [
 	{
 		"add_on" : "AWD Drivetrain",
